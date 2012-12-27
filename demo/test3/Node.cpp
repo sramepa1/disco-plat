fdcf5227e3cad9770873238636cd8b92ffe2fe5a
Ota J. Sandr <ota@R-U-K-A.(none)>
2012-12-27 16:33:36 +0100

    Added third demo - client and server logic are in one executable file

---
+++ demo/test3/Node.cpp
@@ -0,0 +1,119 @@
+
+#define PORT 15672
+
+#include <iostream>
+#include <string>
+#include <sstream>
+
+#include <sys/utsname.h>
+
+#include "Interface.h"
+
+using namespace std;
+
+// globals
+int myPort;
+
+string otherAddr;
+bool haveOther;
+Network_var otherObject;
+
+
+class NetworkImpl : virtual public POA_Network {
+    
+    void joinNetwork(const char* addr) {
+        otherAddr = addr;
+    }
+    
+    void sendInteger(CORBA::Long number) {
+        cout << "Node (port " << myPort << ") - recieved number: " << number << endl;
+    }
+};
+
+
+int main(int argc, char** argv) {
+
+    utsname myUname;
+    uname(&myUname);
+    
+    if(argc == 1) {
+        // first node
+        haveOther = false;
+        myPort = PORT;
+    } else {
+        // second node
+        haveOther = true;
+        myPort = PORT + 1;
+    }
+    
+    stringstream thisAddr;
+    thisAddr << "inet:" << myUname.nodename << ":" << myPort;
+    
+    cout << "Node (port " << myPort << ") - my address: " << thisAddr.str() << endl;
+    
+    int argcORB = 3;
+    char** argvORB = new char*[argcORB];
+    argvORB[0] = argv[0];
+    
+    argvORB[1] = new char[strlen("-ORBIIOPAddr") + 1];
+    memcpy(argvORB[1], "-ORBIIOPAddr", strlen("-ORBIIOPAddr") + 1);
+    
+    argvORB[2] = new char[thisAddr.str().size() + 1];
+    memcpy(argvORB[2], thisAddr.str().c_str(), thisAddr.str().size() + 1);
+    
+    
+    // initialization
+    CORBA::ORB_var orb = CORBA::ORB_init(argcORB, argvORB, "mico-local-orb");
+    CORBA::Object_var obj = orb->resolve_initial_references("RootPOA");
+    PortableServer::POA_var poa = PortableServer::POA::_narrow(obj);
+    PortableServer::POAManager_var mgr = poa->the_POAManager();
+    
+    NetworkImpl* myObject = new NetworkImpl;
+    poa->activate_object(myObject);
+    myObject->_this();
+    
+    
+    if(argc != 1) {
+        // create connection to first node
+        CORBA::Object_var otherObj = orb->bind("IDL:Network:1.0", argv[1]);
+        if (CORBA::is_nil(otherObj)) {
+            cout << "Node (port " << myPort << ") - cannot bind to " << argv[1] << endl;
+            return 1;
+        }
+        
+        otherObject = Network::_narrow(otherObj);
+        otherObject->joinNetwork(thisAddr.str().c_str());
+    }
+    
+    
+    // main loop
+    mgr->activate();
+    cout << "Node (port " << myPort << ") - starting main loop, press CTRL+C for exit!" << endl;
+    while(true) {
+        
+        for(int i = 0; i < 1000; ++i) {
+            if(orb->work_pending()) {
+                orb->perform_work();
+                
+                if(!haveOther && !otherAddr.empty()) {
+                    CORBA::Object_var otherObj = orb->bind("IDL:Network:1.0", otherAddr.c_str());
+                    if (CORBA::is_nil(otherObj)) {
+                        cout << "Node (port " << myPort << ") - cannot bind to " << otherAddr << endl;
+                        return 1;
+                    }
+        
+                    otherObject = Network::_narrow(otherObj);
+                    haveOther = true;
+                }
+            }
+        }
+        
+        int randomNumber = rand();
+        if(haveOther && randomNumber % 50 == 0) {
+            cout << "Node (port " << myPort << ") - sending number: " << randomNumber << endl;
+            otherObject->sendInteger((CORBA::Long)randomNumber);
+        }
+    }
+
+    return 0;
+}
