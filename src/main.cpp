7cf1896634fa7cdcc21006586833089734a258e7
Rusty Horse <smarma.rh@gmail.com>
2012-12-29 09:31:42 +0100

    Actualization of input parameters to keep up witch changed network interface.

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,7 @@ Network* networkModule;
 static struct option long_options[] = {
     {"port", 1, 0, 'p'},
     {"address", 1, 0, 'a'},
+    {"name", 1, 0, 'n'},
     {"file", 1, 0, 'f'},
     {NULL, 0, NULL, 0}
 };
@@ -36,11 +37,13 @@ int main(int argc, char** argv) {
 
     int port = DEFAULT_PORT;
     char* address = NULL;
+    char* name = (char*) "ham0";
+    char* file = NULL;
 
     int character;
     int option_index = 0;
 
-    while ((character = getopt_long(argc, argv, "p:a:", long_options, &option_index)) != -1) {
+    while ((character = getopt_long(argc, argv, "p:a:n:f:", long_options, &option_index)) != -1) {
         switch (character) {
             case 'p' :
                 port = atoi(optarg);
@@ -56,14 +59,35 @@ int main(int argc, char** argv) {
 
                 break;
 
+            case 'n' :
+                {
+                    int lenght = strlen(optarg);
+                    name = new char[lenght + 1];
+                    memcpy(name, optarg, lenght);
+                    name[lenght] = 0;
+                }
+
+            case 'f' :
+                {
+                    int lenght = strlen(optarg);
+                    file = new char[lenght + 1];
+                    memcpy(file, optarg, lenght);
+                    file[lenght] = 0;
+                }
+
+                break;
+
             default:
                 cerr << USAGE_INFO << endl;
                 return ERR_INVALID_ARGUMENTS;
         }
     }
 
+    cout << "Initializing network module with port " << port << ", name '" << name << "' and address '"
+         << (address == NULL ? "NULL" : address) << "'" << endl;
+
     // create connection
-    networkModule = new Network(port, "ham0", address);  // TODO: parse network interface name from args
+    networkModule = new Network(port, name, address);
 
     // some useful work... :)
     sleep(60);
